b20d1e64a259c1ce466fbe9e748ed8c203956ffd
AymanStarlight <laaguid.ayman@gmail.com>
2022-08-14 08:10:45 +0100

    Added Color Helpers to generate shades of Colors

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import seedPalettes from "./Components/seedPalettes";
 import Palette from "./Components/Palette";
+import { generatePalette } from "./colorHelpers";
 
 
 function App() {
+  console.log(generatePalette(seedPalettes[0]))
   return (
     <div>
       <Palette {...seedPalettes[0]} />
